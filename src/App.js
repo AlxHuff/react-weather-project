8b61e159fe262d499a3dac9939f6839ce6aac19e
AlxHuff <alexandra.a.huffman@gmail.com>
2020-10-22 13:50:19 -0400

    added Github repo url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
      Weather App
+     <footer>
+       This project was coded by Alex Huffman and is 
+       <a href="https://github.com/AlxHuff/react-weather-project" 
+       target="_blank">open-sourced on Github</a>
+     </footer>
     </div>
   );
 }
