07bc92413ecdbe1f304c6d066e5ed32bd3cf4e46
AlxHuff <alexandra.a.huffman@gmail.com>
2020-10-23 22:17:59 -0400

    added more CSS styling: Golden Hour theme

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ function App() {
     <div className="App">
       
     <Weather/>
+   
      <footer>
-       This project was coded by Alex Huffman and is 
+       This project was coded by Alex Huffman and is open-sourced on <space/>
        <a href="https://github.com/AlxHuff/react-weather-project" 
-       target="_blank"> open-sourced on Github</a>
+       target="_blank" className="git-link">Github</a>
      </footer>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -7,20 +7,25 @@ export default function Weather (){
     <div className="Weather">
        
        <nav className="navbar navbar-light bg-light justify-content-between">
-  <a className="navbar-brand">React Weather</a>
+  <a className="navbar-brand">Golden Hour Weather</a>
   <form className="form-inline">
     <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
     <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
   </form>
 </nav>
 
+<div className="row"><iframe className="music" src="https://open.spotify.com/embed/album/7f6xPqyaolTiziKf5R5Z0c" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+<div className="weather-card-main">
+
      <h1 className = "weather-title">New York</h1>
+    
      <ul>
          <li>Wednesday 07:00</li>
          <li>Mostly Cloudy</li>
      </ul>
-     <div className ="row">
-         <div className="col-6">
+    
+    <div className ="row">
+            <div className="col-6">
              <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"alt="mostly cloudy"/>
             </div>
              <div className="col-6">
@@ -29,7 +34,12 @@ export default function Weather (){
                  <li>Humidity: 72%</li>
                  <li>Wind: 13 km/hr</li>
              </ul>
-         </div>
-     </div>
+            </div>
+    </div>
+
+</div>
+</div>
+
+    
     </div>)
 }
